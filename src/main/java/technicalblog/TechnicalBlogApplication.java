83245b04bd112417ad6b364a0cbb1ea6d19c4a64
h1m <himanshu.yadav643@gmail.com>
2021-01-04 12:12:08 +0530

    Started the Project

---
+++ src/main/java/technicalblog/TechnicalBlogApplication.java
@@ -0,0 +1,11 @@
+package technicalblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalBlogApplication {
+    public static void main(String [] args) {
+        SpringApplication.run(TechnicalBlogApplication.class, args);
+    }
+}
