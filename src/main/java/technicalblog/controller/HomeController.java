de6d8e14bab653e1f7976299abfc995390e48fa1
h1m <himanshu.yadav643@gmail.com>
2021-01-10 16:49:40 +0530

    removed JDBC API, and introduced JPA implementation by Hibernate

--- src/main/java/technicalblog/controller/HomeController.java
+++ src/main/java/technicalblog/controller/HomeController.java
@@ -9,6 +9,7 @@ import technicalblog.service.PostService;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -19,7 +20,7 @@ public class HomeController {
     @RequestMapping("/")
     public String getAllPosts(Model model) {
 
-        ArrayList<Post> posts = postService.getAllPosts();
+        List<Post> posts = postService.getAllPosts();
 
         model.addAttribute("posts", posts);
 
--- src/main/java/technicalblog/service/PostService.java
+++ src/main/java/technicalblog/service/PostService.java
@@ -3,13 +3,22 @@ package technicalblog.service;
 import org.springframework.stereotype.Service;
 import technicalblog.model.Post;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.TypedQuery;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class PostService {
-    public ArrayList<Post> getAllPosts() {
+
+    @PersistenceUnit(unitName = "techblog")
+    private EntityManagerFactory emf;
+
+    public List<Post> getAllPosts() {
         ArrayList<Post> posts = new ArrayList<Post>();
 
 //        Post post1 = new Post();
@@ -24,30 +33,36 @@ public class PostService {
 //
 //        posts.add(post1);
 //        posts.add(post2);
-        Connection connection = null;
-        try {
-            Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/technicalblog","postgres","himanshu");
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT * FROM posts");
-            while(rs.next()){
-                Post post = new Post();
-                post.setTitle(rs.getString("title"));
-                post.setBody(rs.getString("body"));
-                posts.add(post);
-            }
 
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }finally{
-            try{
-                connection.close();
-            }catch(SQLException e){
-                e.printStackTrace();
-            }
-        }
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<Post> query = em.createQuery("SELECT p FROM Post p", Post.class);
+        List<Post> resultList = query.getResultList();
 
-        return posts;
+
+//        Connection connection = null;
+//        try {
+//            Class.forName("org.postgresql.Driver");
+//            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/technicalblog","postgres","himanshu");
+//            Statement statement = connection.createStatement();
+//            ResultSet rs = statement.executeQuery("SELECT * FROM posts");
+//            while(rs.next()){
+//                Post post = new Post();
+//                post.setTitle(rs.getString("title"));
+//                post.setBody(rs.getString("body"));
+//                posts.add(post);
+//            }
+//
+//        } catch (ClassNotFoundException | SQLException e) {
+//            e.printStackTrace();
+//        }finally{
+//            try{
+//                connection.close();
+//            }catch(SQLException e){
+//                e.printStackTrace();
+//            }
+//        }
+
+        return resultList;
 
     }
 
